1e61f04fa6fe540785b63c351b779fbcdb7f9dcf
cortezgretel <gretel141000@ciencias.unam.com.mx>
2023-11-03 18:44:51 -0600

    añadimos el menu derecho a la actividad de cliente

--- Tarea2/app/src/main/java/com/example/tarea2/ClienteActivity.java
+++ Tarea2/app/src/main/java/com/example/tarea2/ClienteActivity.java
@@ -3,24 +3,35 @@ package com.example.tarea2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ClienteActivity extends AppCompatActivity {
 
     // Agregamos nuestros componentes
+    RadioGroup grupo1;
+    RadioGroup grupo2;
     Button boton1;
     TextView respuesta;
     RadioButton r1, r2;
     CheckBox cb1, cb2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Petty Cliente");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
 
         // Asociamos componentes con sus respectivos IDs
         boton1 = findViewById(R.id.button1);
@@ -29,13 +40,12 @@ public class ClienteActivity extends AppCompatActivity {
         r2 = findViewById(R.id.rdb2);
         cb1 = findViewById(R.id.cb1);
         cb2 = findViewById(R.id.cb2);
+        grupo1=findViewById(R.id.radioGroup);
+        grupo2=findViewById(R.id.radioGroup2);
 
         ImageView imageView = findViewById(R.id.logo);
-        TextView companyNameTextView = findViewById(R.id.nombre);
 
         imageView.setImageResource(R.drawable.logo);
-        companyNameTextView.setText("PETTY");
-
         boton1 = (Button)findViewById(R.id.button1);
         respuesta = (TextView)findViewById(R.id.respuesta);
         r1=(RadioButton)findViewById(R.id.rdb1);
@@ -53,5 +63,60 @@ public class ClienteActivity extends AppCompatActivity {
                 }
             }
         });
+
+        grupo1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            public void onCheckedChanged (RadioGroup group,int checkedId){
+                if (checkedId == R.id.cb1) {
+                    cb1.setChecked(true);
+                    cb2.setChecked(false);
+                } else if (checkedId == R.id.cb2) {
+                    cb1.setChecked(false);
+                    cb2.setChecked(true);
+                }
+            }
+        });
+
+        grupo2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            public void onCheckedChanged (RadioGroup group,int checkedId){
+                if (checkedId == R.id.rdb1) {
+                    r1.setChecked(true);
+                    r2.setChecked(false);
+                } else if (checkedId == R.id.rdb2) {
+                    r1.setChecked(false);
+                    r2.setChecked(true);
+                }
+            }
+        });
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return true;
+    }
+
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.item1){
+            Toast.makeText(this,"Iniciaremos sesión pronto...", Toast.LENGTH_LONG).show();
+        }else if(id == R.id.item2) {
+            Toast.makeText(this, "Ayuda en el servicio de Petty, pronto nos comunicaremos contigo", Toast.LENGTH_LONG).show();
+        }else if(id == R.id.item3) {
+            Toast.makeText(this, "Creadores: Monica Miranda Mijangos, Gretel Penélope Cortés Macias", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Verificar si la ActionBar no es nula antes de llamar a 'show()'
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().show();
+        }
+    }
+
 }
